df65574d255e2de082384e6e57f94dee586c6c4e
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-20 15:39:27 +1100

    updated the scrolltotop function to be on the app not index js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Packages from "./Packages";
 import Contact from "./Contact";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
+      <ScrollToTop />
       <Route exact path="/" component={Home} />
       <Route exact path="/Packages" component={Packages} />
       <Route exact path="/Contact" component={Contact} />
