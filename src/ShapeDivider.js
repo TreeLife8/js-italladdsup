bd6c4493a8dddf5155e617f54f6c1d942a878507
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-23 15:52:57 +1100

    Updated the responsiveness of the shapedivider

--- src/ShapeDivider.js
+++ src/ShapeDivider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ShapeDivider.css";
 
-export default function ShapeDivider() {
+export default function ShapeDivider(props) {
   return (
     <div class="custom-shape-divider-bottom-1634265382">
       <svg
@@ -9,6 +9,7 @@ export default function ShapeDivider() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
+        className={props.height}
       >
         <path
           d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z"
